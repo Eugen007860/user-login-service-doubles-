7ba3e70ca5169be37658d0fc675c088c06346a28
Eugen007860 <eugenhamuraru@gmail.com>
2023-03-10 15:08:25 +0000

    Primer commit

--- src/Application/UserLoginService.php
+++ src/Application/UserLoginService.php
@@ -2,13 +2,45 @@
 
 namespace UserLoginService\Application;
 
+use PHPUnit\Util\Exception;
+use UserLoginService\Domain\User;
+use UserLoginService\Infrastructure\FacebookSessionManager;
+
 class UserLoginService
 {
     private array $loggedUsers = [];
 
-    public function manualLogin(): string
+    public function manualLogin(User $user): void
     {
-        return "user logged";
+        if (in_array($user, $this->getLoggedUsers())) {
+            throw new Exception("User already logged in");
+        }
+        else{
+            $this->loggedUsers[] = $user;
+        }
+    }
+
+    public function getLoggedUsers(): array
+    {
+        return $this->loggedUsers;
+    }
+
+    public function getExternalSessions(): int
+    {
+        $facebookManager = new FacebookSessionManager();
+        return $facebookManager->getSessions();
+    }
+
+    public function login(string $userName, string $password): string
+    {
+        $facebookManager = new FacebookSessionManager();
+        if ($facebookManager->login($userName, $password)){
+            $this->manualLogin(new User($userName));
+            return "Login correcto";
+        }
+        else{
+            return "Login incorrecto";
+        }
     }
 
 }
--- tests/Application/UserLoginServiceTest.php
+++ tests/Application/UserLoginServiceTest.php
@@ -5,17 +5,69 @@ declare(strict_types=1);
 namespace UserLoginService\Tests\Application;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Util\Exception;
 use UserLoginService\Application\UserLoginService;
+use UserLoginService\Domain\User;
+use function PHPUnit\Framework\assertEquals;
+
 
 final class UserLoginServiceTest extends TestCase
 {
     /**
      * @test
      */
-    public function userIsLoggedIn()
+    public function newUserShouldBeLogged()
     {
         $userLoginService = new UserLoginService();
 
-        $this->assertEquals("user logged", $userLoginService->manualLogin());
+        $user = new User("paco");
+
+        $userLoginService->manualLogin($user);
+
+        $this->assertEquals(in_array($user, $userLoginService->getLoggedUsers()), true);
+    }
+
+    /**
+     * @test
+     */
+    public function userIsAlreadyLoggedIn()
+    {
+        $this->expectException(Exception::class);
+
+        $this->expectExceptionMessage("User already logged in");
+
+        $userLoginService = new UserLoginService();
+
+        $user = new User("paco");
+
+        $userLoginService->manualLogin($user);
+
+        $user2 = new User("paco");
+
+        $userLoginService->manualLogin($user2);
+    }
+
+    /**
+     * @test
+    */
+    public function shouldLoginWithFacebook()
+    {
+        $userLoginService = new UserLoginService();
+
+        $loginResult = $userLoginService->login("Paco","Secreto123");
+
+        assertEquals($loginResult, "Login correcto");
+    }
+
+    /**
+     * @test
+     */
+    public function shouldntAllowLoginWithFacebook()
+    {
+        $userLoginService = new UserLoginService();
+
+        $loginResult = $userLoginService->login("Alfredo","123Secreto");
+
+        assertEquals($loginResult, "Login incorrecto");
     }
 }
